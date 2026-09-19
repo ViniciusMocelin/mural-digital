ef6b9ba7d0b6ee1ee4b3ce68ed049c2360b776e2
Vinicius Mocelin <148836485+ViniciusMocelin@users.noreply.github.com>
2026-09-19 15:50:08 -0300

    Removendo a pasta venv

--- mural/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MuralConfig(AppConfig):
-    name = 'mural'
--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Efeito de confirmação dinâmica nos cards
-    const cards = document.querySelectorAll(".card");
-    cards.forEach(card => {
-        card.addEventListener("click", () => {
-            console.log("Card selecionado:", card.querySelector("h3").innerText);
-        });
-    });
-});
